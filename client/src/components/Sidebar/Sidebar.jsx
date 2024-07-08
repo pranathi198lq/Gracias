668f67c347e4607e0a242fd031c89cdbf57175be
pranathi198lq <97428301+pranathi198lq@users.noreply.github.com>
2024-07-08 22:28:00 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, {useContext, useEffect, useState} from 'react'
 import './Sidebar.css'
-import { RssFeed, Chat, PlayCircleFilledOutlined, CollectionsBookmark, Bookmark, HelpOutline, WorkOutline, Event, School} from '@mui/icons-material'
+import { ViewTimeline, Chat, PlayCircleFilledOutlined, CollectionsBookmark, Bookmark, HelpOutline, WorkOutline} from '@mui/icons-material'
 import { AuthContext } from '../../context/AuthContext'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
@@ -12,66 +12,57 @@ export default function Sidebar() {
 
   useEffect(()=>{
     const getFriends= async()=>{
-      try{
-        const friendList= await axios.get(`/users/friends/${user._id}`);
-        setFriends(friendList.data);
-      }catch(err){}
+        try{
+            const friendList= await axios.get(`/users/friends/${user._id}`);
+            setFriends(friendList.data);
+        }catch(err){}
     };
     getFriends();
-  }, [user._id]);
+}, [user._id]);
 
+console.log("Sideher is rendered")
   return (
-    <div className='sidebar'>
+      <div className='sidebar'>
         <div className="wrapper">
             <ul className="list">
                 <li className="item">
-                    <RssFeed className='icon'/>
-                    <span className="text">Feed</span>
+                <Link to={"/profile/"+user.username} style={{textDecoration: "none", color: "black", alignContent: "center", display: "flex", alignItems: "center"   }}>
+                    <ViewTimeline className='icon'/>
+                    <span className="text">Timeline</span>
+                </Link>
                 </li>
                 <li className="item">
+                    <Link to={"/chats"} style={{textDecoration: "none", color: "black", alignContent: "center", display: "flex", alignItems: "center"}}>
                     <Chat className='icon'/>
                     <span className="text">Chats</span>
-                </li>
-                <li className="item">
-                    <PlayCircleFilledOutlined className='icon'/>
-                    <span className="text">Videos</span>
-                </li>
-                <li className="item">
-                    <Link to={"/dairy/"+user.username}>
-                    <CollectionsBookmark className='icon'/>
-                    <span className="text">Gratitude Dairy</span>
                     </Link>
                 </li>
                 <li className="item">
-                    <Link to={"/saved/"+user.username} style={{textDecoration: "none", color: "black", fontSize: "17px", display: "flex", alignItems: "center"}}>
+                    <Link to={"/dairy/"+user.username} style={{textDecoration: "none", color: "black", alignContent: "center", display: "flex", alignItems: "center"}}>
+                    <CollectionsBookmark className='icon'/>
+                    <span className="text">Dairy</span>
+                    </Link>
+                </li>
+                <li className="item">
+                    <Link to={"/saved"+user.username} style={{textDecoration: "none", color: "black", fontSize: "17px", display: "flex", alignItems: "center"}}>
                     <Bookmark className='icon'/>
                     <span className="text">Bookmarks</span>
                     </Link>
                 </li>
                 <li className="item">
+                    <Link to={"/quiz"} style={{textDecoration: "none", color: "black", alignContent: "center", display: "flex", alignItems: "center"}}>
                     <HelpOutline className='icon'/>
-                    <span className="text">Questions</span>
-                </li>
-                <li className="item">
-                    <WorkOutline className='icon'/>
-                    <span className="text">Jobs</span>
-                </li>
-                <li className="item">
-                    <Event className='icon'/>
-                    <span className="text">Events</span>
-                </li>
-                <li className="item">
-                    <School className='icon'/>
-                    <span className="text">Courses</span>
+                    <span className="text">Quiz</span>
+                    </Link>
                 </li>
             </ul>
             <button className="button">Show More</button>
             <hr className='sidehr' />
             <ul className="friendList">
                 {friends.map(friend=>(
-                    <li className="sideItem">
+                    <li key={friend._id} className="sideItem">
                     <Link to={"/profile/"+friend.username} style={{textDecoration: "none", color: "black", fontSize: "17px", display: "flex", alignItems: "center"}}>
-                <img src={friend.profilePicture ? friend.profilePicture : PF+"noPP.jpeg"} alt="" className="sideimg" />
+                <img src={friend.profilePicture ? PF+friend.profilePicture : PF+"noPP.jpeg"} alt="" className="sideimg" />
                 <span className="sideName">{friend.username}</span>
                 </Link>
             </li>
